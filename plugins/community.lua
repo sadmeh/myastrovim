4bd5662778c45f9c842b39677ca329206f424c79
Mehdi <mehdi.sadeghi@apexpmapps.com>
2023-11-14 14:26:50 -0500

    add hop motion

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,36 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.typescript" },
+  {import = "astrocommunity.motion.hop-nvim"},
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+ {
+    "phaazon/hop.nvim",
+    opts = {},
+    keys = {
+      {
+        "f",
+        function() require("hop").hint_words() end,
+        mode = { "n" },
+        desc = "Hop hint words",
+      },
+      {
+        "<S-f>",
+        function() require("hop").hint_lines() end,
+        mode = { "n" },
+        desc = "Hop hint lines",
+      },
+      {
+        "f",
+        function() require("hop").hint_words { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint words",
+      },
+      {
+        "<S-f>",
+        function() require("hop").hint_lines { extend_visual = true } end,
+        mode = { "v" },
+        desc = "Hop hint lines",
+      },
+    },
+  },
 }
